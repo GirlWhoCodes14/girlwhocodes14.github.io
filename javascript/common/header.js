b02db961cab3b1853ad93a8724249426196c9409
GirlWhoCodes14 <alexisayuso12@gmail.com>
2024-06-20 04:47:08 +0800

    attempt to fix footer nav and update contact icons

--- javascript/common/header.js
+++ javascript/common/header.js
@@ -1,9 +1,16 @@
 /* set active page in navbar */
 const navLinks = document.querySelectorAll('.nav-link');
+const footerNav = document.querySelectorAll('.nav-link.footer-nav');
 
 const currentPath = window.location.pathname; // Get the current page path
 
 navLinks.forEach(function(link) {
+  if (link.href.endsWith(currentPath)) {
+    link.classList.add('active');
+  }
+});
+
+footerNav.forEach(function(link) {
   if (link.href.endsWith(currentPath)) {
     link.classList.add('active');
   }
