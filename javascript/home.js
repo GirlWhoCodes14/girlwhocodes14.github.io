e6b480ef893d73a9e9f2feb46f99a15495e95216
GirlWhoCodes14 <alexisayuso12@gmail.com>
2024-07-15 14:24:29 +0800

    move unused files and clean up code

---
+++ javascript/home.js
@@ -0,0 +1,19 @@
+function video_scroll_left() {
+    var left = document.querySelector("#video-scroller");
+    left.scrollBy(-300, 0);
+  }
+
+  function video_scroll_right() {
+    var right = document.querySelector("#video-scroller");
+    right.scrollBy(300, 0);
+  }
+
+  function accomplishments_scroll_left() {
+    var left = document.querySelector("#accomplishments-scroller");
+    left.scrollBy(-350, 0);
+  }
+
+  function accomplishments_scroll_right() {
+    var right = document.querySelector("#accomplishments-scroller");
+    right.scrollBy(350, 0);
+  }
