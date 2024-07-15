4ba3def8902ad34683e9067c8b90a4f2b9d658d7
GirlWhoCodes14 <alexisayuso12@gmail.com>
2024-07-15 19:09:14 +0800

    fix send email bug

--- javascript/contact-message.js
+++ javascript/contact-message.js
@@ -1,55 +1,35 @@
-const scriptURL = 'https://script.google.com/macros/s/AKfycbx9KfUlhPW8Mh2pj1WpTP_nbw1JKU3tByfpnsg9IchYlMH2Z2vskZ8h1YCcH-rC1wh5/exec'
-const form = document.forms['submit-to-google-sheet']
-const msg = document.getElementById("msg")
+const form = document.forms['message-form']
+const msg = document.getElementById("confirm-msg")
 
 form.addEventListener('submit', e => {
     e.preventDefault()
-    fetch(scriptURL, { method: 'POST', body: new FormData(form)})
-    .then(response => {
-        msg.innerHTML = "Your message was sent!"
-        setTimeout(function(){
-        msg.innerHTML = ""
-        }, 5000)
 
-        form.reset()
-    })
-    .catch(error => console.error('Error!', error.message))
+    msg.innerHTML = "Your message is sending. Wait a moment."
+    setTimeout(function(){
+    msg.innerHTML = ""
+    }, 6000)
+
+    form.reset()
 })
 
-
-/*
-const form = document.querySelector("form");
-const fullName = document.getElementById("name");
-const email = document.getElementById("email");
-const msg = document.getElementById("message");
-
 function sendEmail() {
-    const bodyMessage = `Name: ${fullName.value}<br> Email: ${email.value}<br> Message: ${msg.value}`;
-
     Email.send({
-        Host : "smtp.elasticemail.com",
-        Username : "alexis.ayuso.bz@gmail.com",
-        Password : "FE1431E032809F9FF3667C983B33D1DB88A4",
+        SecureToken : "03558de4-6837-4d64-b1a3-8bf078120917",
         To : 'alexis.ayuso.bz@gmail.com',
-        From : "alexis.ayuso.bz@gmail.com",
-        Subject : "Message from Your Website Portfolio",
-        Body : bodyMessage
+        From : "iamalexisayuso@gmail.com",
+        Subject : "Email from " + document.getElementById("name").value + " | Alexis' Website",
+        Body : "Name: " + document.getElementById("name").value
+            + "<br> Email: " + document.getElementById("email").value
+            + "<br><br> Message: <br>" + document.getElementById("message").value
     }).then(
-      message => {
-        if (message = "OK") {
-            Swal.fire({
-                title: "Success!",
-                text: "Email sent successfully!",
-                icon: "success"
-              });
+        message => {
+            if (message = "OK") {
+                Swal.fire({
+                    title: "Success!",
+                    text: "Email sent successfully!",
+                    icon: "success"
+                });
+            }
         }
-      }
     );
 }
-
-form.addEventListener("submit", (e) => {
-    e.preventDefault();
-
-    sendEmail();
-});
-*/
