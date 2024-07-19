9c2016861dc606c9778121ce6e33cd60af782a33
GirlWhoCodes14 <alexisayuso12@gmail.com>
2024-07-19 12:23:28 +0800

    generate project using javascript

---
+++ scripts/portfolio.js
@@ -0,0 +1,134 @@
+const projects = [
+  {
+    link: "/",
+    img: {
+      src: "alexis-website-portfolio-home-page.png",
+      alt: "Alexis Ayuso's Portfolio"
+    },
+    name: 'Alexis Ayuso | Portfolio',
+    type: 'Website'
+  },
+
+  {
+    link: "crossight.html",
+    img: {
+      src: "crossight/crossight_logo.jpg",
+      alt: "CrosSight logo"
+    },
+    name: 'CrosSight',
+    type: 'Android APP'
+  },
+
+  {
+    link: "galexyhub.html",
+    img: {
+      src: "galexy-hub/Shop.jpg",
+      alt: "GaLexy Hub"
+    },
+    name: 'GaLexy Hub',
+    type: 'iOS APP'
+  },
+
+  /*
+  {
+    link: "restaurant-filter.html",
+    img: {
+      src: "restaurant_filter_crop.jpg",
+      alt: "Restaurant Filter"
+    },
+    name: 'Restaurant Filter',
+    type: 'Simple iOS APP'
+  },
+
+  {
+    link: "registration-form.html",
+    img: {
+      src: "registration_form.jpg",
+      alt: "Registration Form"
+    },
+    name: 'Registration Form',
+    type: 'Simple iOS APP'
+  },
+  */
+
+  {
+    link: "temperature-converter.html",
+    img: {
+      src: "temperature-converter/0C to 32F (crop).jpg",
+      alt: "Temperature Converter"
+    },
+    name: 'Temperature Converter',
+    type: 'Simple iOS APP'
+  },
+
+  {
+    link: "sun-moon-lake.html",
+    img: {
+      src: "sun-moon-lake/sun_moon_lake.png",
+      alt: "Sun Moon Lake (3D Terrain)"
+    },
+    name: 'Sun Moon Lake (3D Terrain)',
+    type: '3D Unity Terrain'
+  },
+
+  /*
+  {
+    link: "click-counter.html",
+    img: {
+      src: "click_counter.jpg",
+      alt: "Click Counter"
+    },
+    name: 'Click Counter',
+    type: 'Simple iOS APP'
+  },
+  */
+
+  {
+    link: "movie-invite.html",
+    img: {
+      src: "movie-invite/Invitation (default).jpg",
+      alt: "Movie Night Invitation"
+    },
+    name: 'Movie Night Invitation',
+    type: 'Simple iOS APP'
+  },
+
+  {
+    link: "supreme-court.html",
+    img: {
+      src: "scb/front-left-model.png",
+      alt: "Supreme Court of Belize (3D Model)"
+    },
+    name: 'Supreme Court of Belize',
+    type: 'SketchUp 3D Model'
+  },
+
+  {
+    link: "report-card-generator.html",
+    img: {
+      src: "report_card_generator_final.png",
+      alt: "Report Card Generator"
+    },
+    name: 'Report Card Generator',
+    type: 'C++ Program'
+  }
+
+];
+
+let projectsHTML = '';
+
+projects.forEach(project => {
+  projectsHTML += `
+  <div class="project-preview">
+    <a href="projects/${project.link}">
+      <img src="images/projects/${project.img.src}" alt="${project.img.alt}">
+      <div class="preview-detail">
+        <p>${project.name}</p>
+        <p><span>${project.type}</span></p>
+      </div>
+    </a>
+  </div>
+  `;
+})
+
+document.querySelector('.js-projects').innerHTML = projectsHTML;
