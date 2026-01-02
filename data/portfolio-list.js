23314f3620a3e1be8b0cc4d2f9ae3df8401a69f9
Alexis Ayuso <82748504+GirlWhoCodes14@users.noreply.github.com>
2026-01-01 21:14:00 -0600

    change project imgs to absolute reference

--- data/portfolio-list.js
+++ data/portfolio-list.js
@@ -1,107 +1,71 @@
 export const projects = [
   {
-    link: "../../",
+    link: "/",
     img: {
-      src: "alexis-website-portfolio-home-page.png",
+      src: "/images/projects/website-portfolio-thumbnail.png",
       alt: "Alexis Ayuso's Portfolio"
     },
     name: 'Alexis Ayuso | Portfolio',
     type: 'Website'
   },
   {
-    link: "crossight.html",
+    link: "/projects/crossight.html",
     img: {
-      src: "crossight/crossight_logo.jpg",
+      src: "/images/projects/crossight/crossight_logo.jpg",
       alt: "CrosSight logo"
     },
     name: 'CrosSight',
     type: 'Android APP'
   },
   {
-    link: "galexyhub.html",
+    link: "/projects/galexyhub.html",
     img: {
-      src: "galexy-hub/Shop.jpg",
+      src: "/images/projects/galexy-hub/Shop.jpg",
       alt: "GaLexy Hub"
     },
     name: 'GaLexy Hub',
     type: 'iOS APP'
   },
-
-  /*
   {
-    link: "restaurant-filter.html",
+    link: "/projects/temperature-converter.html",
     img: {
-      src: "restaurant_filter_crop.jpg",
-      alt: "Restaurant Filter"
-    },
-    name: 'Restaurant Filter',
-    type: 'Simple iOS APP'
-  },
-
-  {
-    link: "registration-form.html",
-    img: {
-      src: "registration_form.jpg",
-      alt: "Registration Form"
-    },
-    name: 'Registration Form',
-    type: 'Simple iOS APP'
-  },
-  */
-
-  {
-    link: "temperature-converter.html",
-    img: {
-      src: "temperature-converter/0C to 32F (crop).jpg",
+      src: "/images/projects/temperature-converter/0C to 32F (crop).jpg",
       alt: "Temperature Converter"
     },
     name: 'Temperature Converter',
     type: 'Simple iOS APP'
   },
   {
-    link: "sun-moon-lake.html",
+    link: "/projects/sun-moon-lake.html",
     img: {
-      src: "sun-moon-lake/sun_moon_lake.png",
+      src: "/images/projects/sun-moon-lake/sun_moon_lake.png",
       alt: "Sun Moon Lake (3D Terrain)"
     },
     name: 'Sun Moon Lake (3D Terrain)',
     type: '3D Unity Terrain'
   },
-
-  /*
   {
-    link: "click-counter.html",
+    link: "/projects/movie-invite.html",
     img: {
-      src: "click_counter.jpg",
-      alt: "Click Counter"
-    },
-    name: 'Click Counter',
-    type: 'Simple iOS APP'
-  },
-  */
-
-  {
-    link: "movie-invite.html",
-    img: {
-      src: "movie-invite/Invitation (default).jpg",
+      src: "/images/projects/movie-invite/Invitation (default).jpg",
       alt: "Movie Night Invitation"
     },
     name: 'Movie Night Invitation',
     type: 'Simple iOS APP'
   },
   {
-    link: "supreme-court.html",
+    link: "/projects/supreme-court.html",
     img: {
-      src: "scb/front-left-model.png",
+      src: "/images/projects/supreme-court/scb/front-left-model.png",
       alt: "Supreme Court of Belize (3D Model)"
     },
     name: 'Supreme Court of Belize',
     type: 'SketchUp 3D Model'
   },
   {
-    link: "report-card-generator.html",
+    link: "/projects/report-card-generator.html",
     img: {
-      src: "report_card_generator_final.png",
+      src: "/images/projects/report-card-generator/report_card_generator_final.png",
       alt: "Report Card Generator"
     },
     name: 'Report Card Generator',
